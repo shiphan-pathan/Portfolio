64575675195b06b06b669102834e5b33c4cc4835
shiphan-pathan <shiphanpathan2001@gmail.com>
2024-08-26 14:32:51 +0530

    Portfolio components link  changes Done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,19 +10,22 @@ const Portfolio = () => {
             id: 1,
             logo: javascript,
             name:"JavaScript",
-            discription:"Tic-Tac-Toe"
+            discription:"Tic-Tac-Toe",
+            link: "https://github.com/shiphan-pathan/Tic-Tac-Toe.git"
         },
         {
             id:2,
             logo:java,
             name:"Core Java",
-            discription:"Hotel Reservation System"
+            discription:"Hotel Reservation System",
+            link: "https://github.com/shiphan-pathan/Hotel-Reservation-System.git"
         },
         {
             id:3,
             logo:springboot,
             name:"Spring Boot",
-            discription:"User Authentication System"
+            discription:"User Authentication System",
+            link: "https://github.com/shiphan-pathan/User-Authentication-System.git"
         },
         {
             id:4,
@@ -38,7 +41,7 @@ const Portfolio = () => {
       <span className='underline font-semibold'>Featured Projects</span>
       <div className='grid grid-cols-1 md:grid-cols-4 gap-3 my-5'>
         {
-            cardItem.map(({id,logo,name,discription})=>(
+            cardItem.map(({id,logo,name,discription,link})=>(
                 <div key={id} className='md:w-[300px] md:h-[300px] border-[2px] rounded-lg shadow-lg p-1 cursor-pointer hover:scale-110 duration-300'>
                     <img src={logo} className='w-[120px] h-[120px] p-1 rounded-full border-[2px]' alt="" />
                     <div>
@@ -46,7 +49,8 @@ const Portfolio = () => {
                     <div className='px-2 text-gray-700'>{discription}</div>
                     </div>
                     <div className='px-6 py-4 '>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded'>Source Code</button>
+                    <a href={link} target="_blank" rel="noopener noreferrer" className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
+                    Source Code </a>
                     </div>
                 </div>
             ))
