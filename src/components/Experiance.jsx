170456023e3b7c075c8a914d41c0c9b5159f91d9
shiphan-pathan <shiphanpathan2001@gmail.com>
2026-01-23 17:23:47 +0530

    Exper

--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -8,18 +8,7 @@ const Experiance = () => {
                <div>
                   <h1 className='text-3xl font-bold mb-5'>Experiance</h1>
                   <span>
-                     <span className='font-semibold'>Web Designer & Developer</span> <br />
-                     <span className='font-semibold'>Namastay Digitals LLP, Nashik | April 2025 to Present </span> <br />
-                     <p>Built custom websites using ProcessWire CMS with ProFields: Repeater Matrix for flexible, modular
-                        content management.<br />
-                        Designed responsive, user-friendly frontends using HTML, CSS, JS, and PHP.<br />
-                        Developed custom templates and integrated APIs to meet client-specific needs.<br />
-                        Managed full project lifecycle from design to deployment with a focus on SEO, performance.</p>
-                  </span>
-                  <h1 />
-                  <br />
-                  <span>
-                     <span className='font-semibold'>Freelance Backend Developer</span> <br />
+                     <span className='font-semibold'>Freelance Software Developer</span> <br />
                      <span className='font-semibold'>Freelancer.com | September 2025 – Present</span> <br />
                      <p>Built and deployed custom RESTful APIs using Node.js (TypeScript) and Prisma ORM with MySQL
                         database integration.<br />
@@ -28,6 +17,17 @@ const Experiance = () => {
                         Performed API testing and documentation using Postman and Swagger to ensure accuracy and reliability.<br />
                         Delivered end-to-end backend solutions tailored to client requirements, ensuring clean code and
                         maintainability.</p>
+                  </span>
+                  <h1 />
+                  <br />
+                  <span>
+                     <span className='font-semibold'>Web Designer & Developer</span> <br />
+                     <span className='font-semibold'>Namastay Digitals LLP, Nashik | January 2025 to December 2025</span> <br />
+                     <p>Built custom websites using ProcessWire CMS with ProFields: Repeater Matrix for flexible, modular
+                        content management.<br />
+                        Designed responsive, user-friendly frontends using HTML, CSS, JS, and PHP.<br />
+                        Developed custom templates and integrated APIs to meet client-specific needs.<br />
+                        Managed full project lifecycle from design to deployment with a focus on SEO, performance.</p>
                   </span>
                   <h1 />
                   <br />
